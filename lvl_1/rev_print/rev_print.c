d6aa35adece330f93791604e24a682d4f8615adf
Su Jung Youn <syoun@c4r1p4.42vienna.com>
2024-05-14 13:18:21 +0200

    committed

--- lvl_1/rev_print/rev_print.c
+++ lvl_1/rev_print/rev_print.c
@@ -1,18 +1,21 @@
 #include <unistd.h>
 
-int main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
-	int i = 0;
-	if (argc == 2 && argv[1][i] != '\0')
-	{
-		while (argv[1][i + 1] != '\0')
-			i++;
-		while (i >= 0)
-		{
-			write (1, &argv[1][i], 1);
-			i--;
-		}
-	}
+    int i = 0;
+    if(argc == 2)
+    {
+        while (argv[1][i] != '\0')
+                i++; //i includes the null terminator
+        while (i > 0) //if the condition is i >= 0 it will enter the while loop 1 more time
+					  //and then print smth more?
+        {
+                i--; //to exclude the null terminator
+                write (1, &argv[1][i], 1);
+        }
         write (1, "\n", 1);
-        return (0);
+    }
+    else
+        write (1, "\n", 1);
 }
+
