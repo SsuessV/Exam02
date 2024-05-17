c61c8bbb97b74a75ab10ffc9e366221ce5529fe6
Sue <127782189+SsuessV@users.noreply.github.com>
2024-05-17 11:15:04 +0200

    passed grademe
    shorter than the other one

--- lvl_1/rev_print/rev_print.c
+++ lvl_1/rev_print/rev_print.c
@@ -1,21 +1,19 @@
 #include <unistd.h>
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    int i = 0;
-    if(argc == 2)
-    {
-        while (argv[1][i] != '\0')
-                i++; //i includes the null terminator
-        while (i > 0) //if the condition is i >= 0 it will enter the while loop 1 more time
-					  //and then print smth more?
-        {
-                i--; //to exclude the null terminator
-                write (1, &argv[1][i], 1);
-        }
-        write (1, "\n", 1);
-    }
-    else
-        write (1, "\n", 1);
+	int i = 0;
+	if (argc != 2)
+		write (1, "\n", 1);
+	else
+	{
+		while (argv[1][i] != '\0')
+			i++;
+		while (i > 0)
+		{
+			i--;
+			write (1, &argv[1][i], 1);
+		}
+		write (1, "\n", 1);
+	}
 }
-
